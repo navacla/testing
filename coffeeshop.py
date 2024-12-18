3e687ad82399370d840f133f1a4c486756f0bfde
CN <navacl@gmail.com>
2024-12-18 15:24:45 +0200

    Add new coffee shop program

---
+++ coffeeshop.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+menu = "Black coffee, Cappucino, Latte, Espresso"
+
+print ("Hello stranger\n" + "Welcome to our Coffee shop!\n")
+
+name = input ("What is your name? ")
+
+print ("\nHello " + name + " nice to see you here!")
+
+print ("\nHey " + name + " what would you like? Here is our menu: " + menu + ". Anything you like?\n")
+
+order = input()
+
+print ("\nThank you " + name + " and enjoy your " + order + "!")
