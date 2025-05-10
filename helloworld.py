6a9602adfc83aceaecb3cd118e0bc1625fec78d3
Claudio <claudio@miniMacM4pro.local>
2025-05-10 14:04:27 +0300

    testing

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,13 @@
 # This program prints Hello, world!
 
-print('Hello, world!')
+greeting : str = 'Hello, world!' 
+
+print(greeting)
+
+pie_list = ['Banana', 'Blueberry', 'Peacan', 'Orange']
+
+for name in pie_list:
+    if len(name) < 6:
+        print(name)
+else:
+        print("Name too long")
